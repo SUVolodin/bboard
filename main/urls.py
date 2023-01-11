713782c0ccfb32f5540809ba5261b5971df8fb53
Stanislav Volodin <volodin.stas87@gmail.com>
2023-01-11 18:09:47 +0300

    Сайт объявлений с сипользованием Bootstrap

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from .views import other_page
+from django.urls import path
+
+from .views import index
+
+app_name = 'main'
+urlpatterns = [
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index')
+]
